283308d806ac9c0096af6fb9b6c339f09fc367ac
Emre Kılıç <emre1kilic@outlook.com>
2022-02-03 18:06:55 +0300

    Add project files.

---
+++ customer-ops/Program.cs
@@ -0,0 +1,9 @@
+﻿using Npgsql;
+
+class Program
+{
+    public static void Main(String[] args) {
+        var db = "Host=localhost;Username=postgres;Password=emr2001129000;Database=CustomerOpsDatabase";
+
+    }
+}
